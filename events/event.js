f4802acc819841803363ebd1543240ce8ced4a64
Lakshmi prasanna <shamballilakshmiprasann@gmail.com>
2021-12-21 13:59:30 +0530

    commit

---
+++ events/event.js
@@ -0,0 +1,31 @@
+
+function lucky()
+{
+document.getElementById('demo').style.backgroundColor="blue";
+
+}
+function second()
+{
+document.getElementById('two').style.backgroundColor="blue";
+
+}
+function third()
+{
+document.getElementById('three').style.backgroundColor="blue";
+
+}
+function fourth()
+{
+document.getElementById('four').style.backgroundColor="blue";
+
+}
+function fifth()
+{
+document.getElementById('five').style.backgroundColor="blue"
+
+}
+function six()
+{
+document.getElementById('six').style.backgroundColor="blue";
+
+}
